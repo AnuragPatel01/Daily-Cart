7d26b70e048def9e3af3c72be1004550712dfe34
Anurag Patel <ap23492001@gmail.com>
2023-08-04 01:26:52 +0530

    responsive

--- myntra.js
+++ myntra.js
@@ -1,47 +1,77 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+const loginTab = document.getElementById('loginTab');
+const logoutTab = document.getElementById('logoutTab');
+const cartTab = document.getElementById('lg-bag');
 
 
-// script.js
+if (bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+let loggedIn = false;
+
+
+// Function to toggle login/logout tabs
+function toggleTabs() {
+    if (loggedIn) {
+        loginTab.style.display = 'none';
+        logoutTab.style.display = 'block';
+        cartTab.style.display = 'block';
+    } else {
+        loginTab.style.display = 'block';
+        logoutTab.style.display = 'none';
+        cartTab.style.display = 'none';
+    }
+}
+
+// Event listener for login button
+loginTab.addEventListener('click', () => {
+    loggedIn = true;
+    toggleTabs();
+    
+      
+      
+    // You can also perform additional actions like showing user-specific content
+    content.innerHTML = '<p>Welcome, user!</p>';
+});
+
+
+// Event listener for logout button
+logoutTab.addEventListener('click', () => {
+    loggedIn = false;
+    toggleTabs();
+    // Clear user-specific content
+    content.innerHTML = '';
+    
+});
+
+// Initialize tabs based on login status
+toggleTabs();
 
 
 
-  const nav = document.getElementById('navbar');
-        const loginModal = document.getElementsByClassName('login-container');
-        const close = document.getElementById('close');
-        const loginForm = document.getElementById('login-form');
-        const errorMessage = document.getElementById('error-message');
 
-        // Open login modal
-        document.getElementById('bar').addEventListener('click', () => {
-            nav.classList.add('active');
-            loginModal.style.display = 'block';
-        });
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    
+    if (username === 'anuragpatel' && password === '123456') {
+      window.location.href = '/index.html';
+    } else {
+      alert('Invalid username or password. Please try again.');
+    }
+  });
 
-        // Close login modal
-        if (close) {
-            close.addEventListener('click', () => {
-                nav.classList.remove('active');
-                loginModal.style.display = 'none';
-                errorMessage.textContent = ''; // Clear error message
-            });
-        }
-
-        // Handle login form submission
-        
-        if (loginForm) {
-          loginForm.addEventListener('submit', (event) => {
-              event.preventDefault();
-
-              const username = document.getElementById('username').value;
-              const password = document.getElementById('password').value;
-
-              if (username === 'anuragpatel' && password === '123456') {
-                  // Simulate successful login by displaying a message
-                  errorMessage.textContent = 'Login successful. Redirecting...';
-                  setTimeout(() => {
-                      window.location.href = '/index.html'; // Redirect after a delay
-                  }, 2000); // Delay in milliseconds
-              } else {
-                  errorMessage.textContent = 'Invalid username or password. Please try again.';
-              }
-            });
-        }
