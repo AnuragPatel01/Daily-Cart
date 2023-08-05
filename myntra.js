2fabd5aa8b7962e48227b8f25576fabe5ad13dad
Anurag Patel <ap23492001@gmail.com>
2023-08-06 00:23:56 +0530

    adding logout

--- myntra.js
+++ myntra.js
@@ -4,24 +4,22 @@ const nav = document.getElementById('navbar');
 const loginTab = document.getElementById('loginTab');
 const logoutTab = document.getElementById('logoutTab');
 const cartTab = document.getElementById('lg-bag');
+// const content = document.getElementById('content'); // Assuming you have an element with this ID
 
-
-if (bar){
+if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('active');
-    })
+    });
 }
 
-if (close){
+if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active');
-    })
+    });
 }
 
 let loggedIn = false;
 
-
-// Function to toggle login/logout tabs
 function toggleTabs() {
     if (loggedIn) {
         loginTab.style.display = 'none';
@@ -34,44 +32,29 @@ function toggleTabs() {
     }
 }
 
-// Event listener for login button
 loginTab.addEventListener('click', () => {
     loggedIn = true;
     toggleTabs();
-    
-      
-      
-    // You can also perform additional actions like showing user-specific content
     content.innerHTML = '<p>Welcome, user!</p>';
 });
 
-
-// Event listener for logout button
 logoutTab.addEventListener('click', () => {
     loggedIn = false;
     toggleTabs();
-    // Clear user-specific content
     content.innerHTML = '';
-    
 });
 
-// Initialize tabs based on login status
 toggleTabs();
 
-
-
-
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
+document.getElementById('login-form').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent form submission
+    
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-  
     
     if (username === 'anuragpatel' && password === '123456') {
-      window.location.href = '/index.html';
+        window.location.href = '/index.html'; // Redirect to the correct path
     } else {
-      alert('Invalid username or password. Please try again.');
+        alert('Invalid username or password. Please try again.');
     }
-  });
-
+});
