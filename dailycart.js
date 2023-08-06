17aebdc94aa65bb82f002d4a6081c741d5d222cc
Anurag Patel <ap23492001@gmail.com>
2023-08-06 21:54:37 +0530

    revert

---
+++ dailycart.js
@@ -0,0 +1,30 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    
+    if (username === 'anuragpatel' && password === '123456') {
+      window.location.href = '/Daily-Cart/home.html';
+    } else {
+      alert('Invalid username or password. Please try again.');
+    }
+  });
+  
